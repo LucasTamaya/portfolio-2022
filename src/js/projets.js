5da0e1eab786de4f35c557c43a2d642af9423674
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-02-26 00:46:35 +0400

    second commit

--- src/js/projets.js
+++ src/js/projets.js
@@ -2,10 +2,6 @@ const homeIcon = document.getElementById("homeIcon");
 const menuIcon = document.getElementById("menuIcon");
 const nav = document.getElementById("nav");
 
-const item1 = document.getElementById("item1");
-const item2 = document.getElementById("item2");
-const item3 = document.getElementById("item3");
-
 let isMenuOpen = false;
 // ouvre et ferme le menu
 menuIcon.addEventListener("click", () => {
@@ -16,9 +12,6 @@ menuIcon.addEventListener("click", () => {
       nav.classList.toggle("navContainer-visible");
     }, 600);
     menuIcon.className = "fa-solid fa-xmark headerContainer-icon";
-    item1.id = "item1-out";
-    item2.id = "item2-out";
-    item3.id = "item3-out";
     return (isMenuOpen = true);
   }
 
@@ -26,9 +19,6 @@ menuIcon.addEventListener("click", () => {
   if (isMenuOpen) {
     nav.classList.toggle("navContainer-visible");
     menuIcon.className = "fa-solid fa-bars headerContainer-icon";
-    item1.id = "item1-in";
-    item2.id = "item2-in";
-    item3.id = "item3-in";
     return (isMenuOpen = false);
   }
 });
